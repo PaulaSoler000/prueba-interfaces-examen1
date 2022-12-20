8322e56bd97ddbd5604b21c057caa3ce09df5381
PaulaSoler000 <paulawei.s249952@cesurformacion.com>
2022-12-20 20:41:16 +0100

    .

--- examen1trimestre2/ExamenDIW2022-main/static/app.js
+++ examen1trimestre2/ExamenDIW2022-main/static/app.js
@@ -1,3 +1,7 @@
 // nombre_del_alumno
-//
+let el = document.querySelector("#login [data-dialog]")
+el.addEventListener("click",function(){
+    let nombre = document.querySelector("input[name=nombre]").value
+    document.querySelector("a.menu_usuario").innerText = nombre
+})
 // Completar JS aqui
